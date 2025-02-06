8679c31fd7c74ff8e6feee0557dc6c4b365b8075
Scrappyz <ghostmic3000@gmail.com>
2025-02-06 20:44:51 +0800

    Added Java Spring Boot

---
+++ java-spring/src/main/java/com/example/!artifact!/!main!Application.java
@@ -0,0 +1,13 @@
+package com.example.!artifact!;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class !main!Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(!main!Application.class, args);
+	}
+
+}
